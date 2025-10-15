132e74b64dffdef84e6cb6bbe17f0bbe1689a9b0
tim-dev-docs <tim@dev-docs.io>
2025-10-15 15:30:14 -0700

    dev fix

--- forge.config.js
+++ forge.config.js
@@ -29,10 +29,6 @@ module.exports = {
       },
     },
     {
-      name: '@electron-forge/maker-zip',
-      platforms: ['darwin'],
-    },
-    {
       name: '@electron-forge/maker-deb',
       config: {},
     },
