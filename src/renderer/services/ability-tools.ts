fac129652e66a6a4a6d697d2b1a41e11ed04abf0
avb-is-me <andrew.ryan.vanbeek@gmail.com>
2025-11-12 16:26:23 -0800

    working state again

--- src/renderer/services/ability-tools.ts
+++ src/renderer/services/ability-tools.ts
@@ -9,6 +9,10 @@ export const toolsToAbilities = {
         command: 'plan',
         description: 'use this ability to plan the steps necessary to complete the task.  Generates a planning token.  Make sure to run run-code after planning.',
       },
+      {
+        command: 'web-search',
+        description: 'use this ability to search the web for information.  Feel free to run this after planning so can gather more information more info before you run-code',
+      },
     ],
   },
 }
